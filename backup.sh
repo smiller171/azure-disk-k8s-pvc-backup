fcd656ae31a6db43c5d5ecc2ea84328a6d0dad86
Scott Miller <scott@millergeek.xyz>
2020-06-11 19:35:00 -0400

    login with service principal

--- backup.sh
+++ backup.sh
@@ -1,7 +1,14 @@
 #!/bin/bash -ex
 
-TARGET_VOLUME=$1
-az login --identity
+SECRET=$1
+TARGET_VOLUME=$2
+
+SECRET_DATA=$(kubectl -n test get secret azure-service-principal -o json | jq .data)
+APP_ID=$(echo $SECRET_DATA | jq -r .appId)
+TENANT=$(echo $SECRET_DATA | jq -r .tenant)
+PASSWORD=$(echo $SECRET_DATA | jq -r .password)
+
+az login --service-principal -u ${APP_ID} -p ${PASSWORD} --tenant ${TENANT}
 VOLUME_NAME=$(kubectl get pvc ${TARGET_VOLUME} -o json | jq -r '.spec.volumeName')
 DISK_ID=$(az disk list --query "[].id | [?contains(@,\`${VOLUME_NAME}\`)]" -o tsv)
 RESOURCE_GROUP=$(echo ${DISK_ID} | cut -d "/" -f 5)
