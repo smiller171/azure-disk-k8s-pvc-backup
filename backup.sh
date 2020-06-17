38ab455ada2407cf3d5277256b10368f0ac517f4
Scott Miller <scott@millergeek.xyz>
2020-06-17 14:15:50 -0400

    add subscription selection

--- backup.sh
+++ backup.sh
@@ -1,14 +1,16 @@
-#!/bin/bash -ex
+#!/bin/bash -e
 
 SECRET=$1
 TARGET_VOLUME=$2
 
-SECRET_DATA=$(kubectl get secret azure-service-principal -o json | jq .data)
-APP_ID=$(echo $SECRET_DATA | jq -r .appId | base64 -d)
-TENANT=$(echo $SECRET_DATA | jq -r .tenant | base64 -d)
-PASSWORD=$(echo $SECRET_DATA | jq -r .password | base64 -d)
+SECRET_DATA=$(kubectl get secret ${SECRET} -o json | jq .data)
+APP_ID=$(echo ${SECRET_DATA} | jq -r .appId | base64 -d)
+TENANT=$(echo ${SECRET_DATA} | jq -r .tenant | base64 -d)
+PASSWORD=$(echo ${SECRET_DATA} | jq -r .password | base64 -d)
+SUBSCRIPTION=$(echo ${SECRET_DATA} | jq -r .subscription | base64 -d)
 
 az login --service-principal -u=${APP_ID} -p ${PASSWORD} --tenant ${TENANT}
+az account set -s ${SUBSCRIPTION}
 VOLUME_NAME=$(kubectl get pvc ${TARGET_VOLUME} -o json | jq -r '.spec.volumeName')
 DISK_ID=$(az disk list --query "[].id | [?contains(@,\`${VOLUME_NAME}\`)]" -o tsv)
 RESOURCE_GROUP=$(echo ${DISK_ID} | cut -d "/" -f 5)
